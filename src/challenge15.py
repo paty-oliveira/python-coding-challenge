aaffb23025c25a21bf42076ecb64700833055372
paty-oliveira <patriciia.mota@gmail.com>
2021-07-16 16:14:51 +0100

    Implement Up and Down challenge

--- src/challenge15.py
+++ src/challenge15.py
@@ -11,5 +11,5 @@
 """
 
 
-def up_down(param):
-    pass
+def up_down(number):
+    return number - 1, number + 1
--- tests/test_challenge15.py
+++ tests/test_challenge15.py
@@ -1,9 +1,29 @@
 import unittest
+from src.challenge15 import up_down
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestChallenge15(unittest.TestCase):
+
+    def test_should_return_zero_as_lower_number_and_2_as_higher_number(self):
+        input_param = 1
+        current_result = up_down(input_param)
+        expected_result = (0, 2)
+
+        self.assertEqual(current_result, expected_result)
+
+    def test_should_return_four_as_lower_number_and_six_as_higher_number(self):
+        input_param = 5
+        current_result = up_down(input_param)
+        expected_result = (4, 6)
+
+        self.assertEqual(current_result, expected_result)
+
+    def test_should_return_minus_2_as_lower_number_and_zero_as_higher_number(self):
+        input_param = -1
+        current_result = up_down(input_param)
+        expected_result = (-2, 0)
+
+        self.assertEqual(current_result, expected_result)
 
 
 if __name__ == '__main__':
