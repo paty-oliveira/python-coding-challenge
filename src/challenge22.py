4ced2f382d4b359cee9df9722514b27572224b16
paty-oliveira <patriciia.mota@gmail.com>
2021-07-20 23:14:01 +0100

    Implement List xor challenge

--- src/challenge22.py
+++ src/challenge22.py
@@ -17,5 +17,10 @@
 """
 
 
-def list_xor(param):
-    pass
+def list_xor(n, list1, list2):
+    if n in list1 and n not in list2:
+        return True
+    elif n in list2 and n not in list1:
+        return True
+    elif n in list1 and n in list2:
+        return False
--- tests/test_challenge22.py
+++ tests/test_challenge22.py
@@ -1,9 +1,40 @@
 import unittest
+from src.challenge22 import list_xor
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestChallenge22(unittest.TestCase):
+
+    def test_should_return_true_when_one_is_in_first_list(self):
+        input_param1 = 1
+        input_param2 = [1]
+        input_param3 = [0]
+        current_result = list_xor(input_param1, input_param2, input_param3)
+
+        self.assertTrue(current_result)
+
+    def test_should_return_true_when_one_is_in_second_list(self):
+        input_param1 = 1
+        input_param2 = [0]
+        input_param3 = [1]
+        current_result = list_xor(input_param1, input_param2, input_param3)
+
+        self.assertTrue(current_result)
+
+    def test_should_return_false_when_one_is_in_two_list_simultaneous(self):
+        input_param1 = 1
+        input_param2 = [1]
+        input_param3 = [1]
+        current_result = list_xor(input_param1, input_param2, input_param3)
+
+        self.assertFalse(current_result)
+
+    def test_should_return_false_whe_one_is_not_in_any_list(self):
+        input_param1 = 1
+        input_param2 = [2, 3, 4]
+        input_param3 = [5, 6, 7]
+        current_result = list_xor(input_param1, input_param2, input_param3)
+
+        self.assertFalse(current_result)
 
 
 if __name__ == '__main__':
