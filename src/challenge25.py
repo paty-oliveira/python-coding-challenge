0b0f65c477e8adf7778cb32b4a52c461692e4cf3
paty-oliveira <patriciia.mota@gmail.com>
2021-08-17 10:48:38 +0100

    Implement Who Likes it challenge

--- src/challenge25.py
+++ src/challenge25.py
@@ -1,10 +1,10 @@
 """
 You probably know the "like" system from Facebook and other pages.
 People can "like" blog posts, pictures or other items.
- We want to create the text that should be displayed next to such an item.
+We want to create the text that should be displayed next to such an item.
 
 Implement the function likes which takes an array containing the names of people that like an item.
- It must return the display text as shown in the examples:
+It must return the display text as shown in the examples:
 
 likes([]) # must be "no one likes this"
 likes(["Peter"]) # must be "Peter likes this"
@@ -16,5 +16,17 @@ For 4 or more names, the number in and 2 others simply increases.
 """
 
 
-def likes(names):
-    pass
+def likes(names: list):
+    standard_response = {
+        0: "no one likes this",
+        1: "{} likes this",
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this"
+    }
+    number_likes = len(names)
+
+    if number_likes <= 3:
+        return standard_response.get(number_likes).format(*names)
+
+    else:
+        return "{}, {} and {number} others like this".format(*names, number=str(number_likes-2))
--- tests/test_challenge25.py
+++ tests/test_challenge25.py
@@ -1,9 +1,50 @@
 import unittest
+from src.challenge25 import likes
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestChallenge25(unittest.TestCase):
+
+    def test_should_return_no_likes_when_names_list_is_empty(self):
+        input_names = []
+        actual_result = likes(input_names)
+        expected_result = "no one likes this"
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_should_return_one_name_who_likes_the_post(self):
+        input_names = ["Peter"]
+        actual_result = likes(input_names)
+        expected_result = "Peter likes this"
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_should_return_two_names_who_like_the_post(self):
+        input_names = ["Jacob", "Alex"]
+        actual_result = likes(input_names)
+        expected_result = "Jacob and Alex like this"
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_should_return_three_names_who_like_the_post(self):
+        input_names = ["Max", "John", "Mark"]
+        actual_result = likes(input_names)
+        expected_result = "Max, John and Mark like this"
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_should_return_four_names_who_like_the_post(self):
+        input_names = ["Alex", "Jacob", "Mark", "Max"]
+        actual_result = likes(input_names)
+        expected_result = "Alex, Jacob and 2 others like this"
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_should_return_five_names_who_like_the_post(self):
+        input_names = ["Alex", "Jacob", "Mark", "Max", "Patricia"]
+        actual_result = likes(input_names)
+        expected_result = "Alex, Jacob and 3 others like this"
+
+        self.assertEqual(actual_result, expected_result)
 
 
 if __name__ == '__main__':
